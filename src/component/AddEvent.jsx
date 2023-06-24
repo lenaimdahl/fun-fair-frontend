00fd739b3a12ce12418fae11452a92a2ff0daff0
olgam <olgamartyniuk88@gmail.com>
2023-06-24 16:26:28 +0200

    updating profile page

--- src/component/AddEvent.jsx
+++ src/component/AddEvent.jsx
@@ -1,7 +1,22 @@
+import NewEvent from "./NewEvent";
+
 function AddEvent() {
   return (
-    <div>
-      <h1>Add an Event for today</h1>
+    <div className="add-event-box">
+      <h2>Add an Event for today</h2>
+      {/* <form onSubmit={handleAddEvent}> */}
+      <form>
+        <label>events: </label>
+        {/* these need to be populated from our database */}
+        <select id="event" name="event">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="fiat">Fiat</option>
+          <option value="audi">Audi</option>
+        </select>
+        <button type="submit">Add an event</button>
+      </form>
+      <NewEvent />
     </div>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,21 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import AddEvent from "../component/AddEvent";
 
 import Calendar from "../component/Calendar";
+import MoodSelection from "../component/MoodSelection";
 
 function Profile() {
   const { logOutUser, user } = useContext(AuthContext);
 
   return (
     <div>
-      {user && user.email}
+      {/* {user && user.email} */}
       <div className="calendar-flex">
         <Calendar />
         <div className="right-side-menu">
-          <div>
-            <h2>Add an activity</h2>
-          </div>
+          <MoodSelection />
+          <AddEvent />
         </div>
       </div>
 
