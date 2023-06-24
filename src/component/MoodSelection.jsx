34282d566d6d78898c00c1d5dd9262318f59144c
Lena Imdahl <mail@lenaimdahl.de>
2023-06-24 21:06:54 +0200

    fixed sending token

--- src/component/MoodSelection.jsx
+++ src/component/MoodSelection.jsx
@@ -2,30 +2,62 @@ import { BackendAPI } from "../api/BackendAPIHandler";
 import { useState } from "react";
 
 function MoodSelection() {
-  const [selectedEmoji, setSelectedEmoji] = useState("");
+  const [mood, setMood] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [showMoodCard, setShowMoodCard] = useState(true);
 
   const backendAPIInstance = new BackendAPI();
 
-  const handleMoodSelection = async (emoji) => {
-    setSelectedEmoji(emoji);
-    const today = new Date().setHours(0, 0, 0, 0);
+  const handleMoodSelection = async (type) => {
+    const currentDay = new Date().setHours(0, 0, 0, 0);
     try {
-      await backendAPIInstance.saveMood(emoji, today);
+      await backendAPIInstance.saveMood(type, currentDay);
     } catch (error) {
       console.error(error);
     }
   };
+
+  // function handleMoodSelection(mood) {
+  //   setIsLoading(true);
+  //   apiService
+  //     .updateMood({ mood })
+  //     .then((updatedMoods) => {
+  //       // use the updated mood from the response immediately
+  //       setMood(updatedMoods.data);
+  //       // hide the mood card after submit, "mood card timeout"
+  //       setShowMoodCard(false);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }
 
   return (
     <div>
       <div id="mood-selection">
         <h2>Select your mood</h2>
         <div className="mood-container">
-          <span onClick={() => handleMoodSelection("😊")}>😊</span>
-          <span onClick={() => handleMoodSelection("😔")}>😔</span>
-          <span onClick={() => handleMoodSelection("😡")}>😡</span>
-          <span onClick={() => handleMoodSelection("😍")}>😍</span>
-          <span onClick={() => handleMoodSelection("😴")}>😴</span>
+          <button onClick={() => handleMoodSelection("happy")}>
+            😊
+            <p>happy</p>
+          </button>
+          <button onClick={() => handleMoodSelection("sad")}>
+            😔
+            <p>sad</p>
+          </button>
+          <button onClick={() => handleMoodSelection("angry")}>
+            😡
+            <p>angry</p>
+          </button>
+          <button onClick={() => handleMoodSelection("in love")}>
+            😍
+            <p>in love</p>
+          </button>
+          <button onClick={() => handleMoodSelection("sleepy")}>
+            😴
+            <p>sleepy</p>
+          </button>
         </div>
       </div>
     </div>
