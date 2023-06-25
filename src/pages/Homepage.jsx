deb715d26573c7f7c85c9ffd37a3acb04add9883
Lena Imdahl <mail@lenaimdahl.de>
2023-06-25 20:41:09 +0200

    added function to show 1 activity for 24 hours and send it to backend

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,57 @@
 import { useEffect, useState } from "react";
 import { BoredAPI } from "../api/BoredAPIHandler";
+import { BackendAPI } from "../api/BackendAPIHandler";
 import Preview from "../component/Preview";
 import CalenderPreview from "../assets/calendar-example.png";
 
 function HomePage() {
   const [activity, setActivity] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
   const boredAPIInstance = new BoredAPI();
+  const backendAPIInstance = new BackendAPI();
 
   useEffect(() => {
-    getActivityData();
+    const storedActivity = localStorage.getItem("activity");
+    const storedDate = localStorage.getItem("date");
+
+    // Check if stored activity exists and if it was stored within the last 24 hours
+    if (storedActivity && storedDate && isWithinLast24Hours(storedDate)) {
+      setActivity(storedActivity);
+    } else {
+      fetchActivity();
+    }
   }, []);
 
-  const getActivityData = async () => {
-    const data = await boredAPIInstance.getActivity();
-    setActivity(data);
+  const isWithinLast24Hours = (dateString) => {
+    const storedDate = new Date(dateString);
+    const currentDate = new Date();
+    const timeDifference = currentDate - storedDate;
+    const hoursDifference = timeDifference / (1000 * 60 * 60);
+    return hoursDifference < 24;
+  };
+
+  const fetchActivity = async () => {
+    const { activity } = await boredAPIInstance.getActivity();
+    setActivity(activity);
+    localStorage.setItem("activity", activity);
+    localStorage.setItem("date", new Date().toString());
+    sendActivityToBackend(activity);
+  };
+
+  const sendActivityToBackend = async (activity) => {
+    setIsLoading(true);
+    try {
+      const payload = {
+        title: activity,
+      };
+
+      await backendAPIInstance.saveActivity(payload);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -29,7 +67,7 @@ function HomePage() {
         <div className="activity-container">
           <div>
             <h1>Activity tip of the day:</h1>
-            <h2>"{activity.activity}"</h2>
+            <h2>"{activity}"</h2>
           </div>
           <button className="signup-button">Sign up!</button>
         </div>
