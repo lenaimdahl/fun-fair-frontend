2bb24bbabcada56c9d9be77618231d0645de5de0
olgam <olgamartyniuk88@gmail.com>
2023-06-27 12:16:48 +0200

    fixing new event but still not working

--- src/component/NewEvent.jsx
+++ src/component/NewEvent.jsx
@@ -1,34 +1,49 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BackendAPI } from "../api/BackendAPIHandler";
 
 function NewEvent() {
-    // const [title, setTitle] = useState("");
-    // const [image, setImage] = useState("");
-    // const [points, setPoints] = useState("");
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [points, setPoints] = useState("");
 
-    // const handleTitle = (event) => setTitle(event.target.value);
-    // const handleImage = (event) => setImage(event.target.value);
-    // const handlePoints = (event) => setPoints(event.target.value);
+  const handleTitle = (event) => setTitle(event.target.value);
+  const handleImage = (event) => setImage(event.target.value);
+  const handlePoints = (event) => setPoints(event.target.value);
 
-    return (
-      <div className="new-event-box">
-        <h2>Create a new Event</h2>
-        {/* <form onSubmit={handleNewEvent}> */}
-        {/* <form>
+  const navigate = useNavigate();
+
+  const backendAPIInstance = new BackendAPI();
+
+  const handleAddEvent = async (e) => {
+    e.preventDefault();
+
+    const currentDay = new Date().setHours(0, 0, 0, 0);
+
+    try {
+      let newEvent = {
+        title,
+        image,
+        points,
+        timestamp: currentDay,
+      };
+
+      await backendAPIInstance.saveEvent(newEvent);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="new-event-box">
+      <h2>Create a new Event</h2>
+      <form onSubmit={handleAddEvent}>
         <label>title:</label>
-        <input
-          type="title"
-          name="title"
-          value={title}
-          onChange={handleTitle}
-        />
-          <label>emoji:</label>
-        <input
-          type="image"
-          name="image"
-          value={image}
-          onChange={handleImage}
-        />
-        
+        <input type="text" name="title" value={title} onChange={handleTitle} />
+        <label>emoji:</label>
+        <input type="text" name="image" value={image} onChange={handleImage} />
+
         <label>points:</label>
         <input
           type="points"
@@ -37,9 +52,9 @@ function NewEvent() {
           onChange={handlePoints}
         />
         <button type="submit">Add!</button>
-        </form> */}
-      </div>
-    );
-  }
-  
-  export default NewEvent;
+      </form>
+    </div>
+  );
+}
+
+export default NewEvent;
