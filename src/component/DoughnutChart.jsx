e851714d2fd8daf0d64eda15ef014f91da3806b9
Lena Imdahl <mail@lenaimdahl.de>
2023-07-01 14:51:15 +0200

    changed background color of doughnut chart

--- src/component/DoughnutChart.jsx
+++ src/component/DoughnutChart.jsx
@@ -85,11 +85,11 @@ function DoughnutChart({ moods }) {
     );
     const data = percentages.map((mood) => mood.percentage);
     const backgroundColor = [
-      "rgba(255, 99, 132, 0.2)",
-      "rgba(54, 162, 235, 0.2)",
-      "rgba(255, 206, 86, 0.2)",
-      "rgba(75, 192, 192, 0.2)",
-      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 99, 132, 0.5)",
+      "rgba(54, 162, 235, 0.5)",
+      "rgba(255, 206, 86, 0.5)",
+      "rgba(75, 192, 192, 0.5)",
+      "rgba(153, 102, 255, 0.5)",
     ];
     const borderColor = [
       "rgba(255, 99, 132, 1)",
