9b369829401fd794133d9966c8f8eddaa7fb76f6
Lena Imdahl <mail@lenaimdahl.de>
2023-07-01 14:34:01 +0200

    removed handleHover and handleLeave for doughnut chart

--- src/component/DoughnutChart.jsx
+++ src/component/DoughnutChart.jsx
@@ -18,33 +18,12 @@ export const options = {
         color: "#F5F2FF",
         padding: 30,
       },
-      onHover: handleHover,
-      onLeave: handleLeave,
     },
   },
   title: {
     display: false,
   },
 };
-
-function handleHover(evt, item, legend) {
-  legend.chart.data.datasets[0].backgroundColor.forEach(
-    (color, index, colors) => {
-      colors[index] =
-        index === item.index || color.length === 9 ? color : color + "4D";
-    }
-  );
-  legend.chart.update();
-}
-// doughnut hover behavior: Removes the alpha channel from background colors
-function handleLeave(evt, item, legend) {
-  legend.chart.data.datasets[0].backgroundColor.forEach(
-    (color, index, colors) => {
-      colors[index] = color.length === 9 ? color.slice(0, -2) : color;
-    }
-  );
-  legend.chart.update();
-}
 
 function DoughnutChart({ moods }) {
   const [isLoading, setIsLoading] = useState(true);
