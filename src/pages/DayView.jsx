26c4706545db99c9559a00ae3307f0bc390e0dc7
Lena Imdahl <mail@lenaimdahl.de>
2023-07-11 18:35:46 +0200

    added newText to dayView

--- src/pages/DayView.jsx
+++ src/pages/DayView.jsx
@@ -1,7 +1,39 @@
-import "../css/calendar.scss";
+import "../css/calendar.css";
+import React, { useContext } from "react";
+import { useState } from "react";
+import NewText from "../component/NewText";
 
 function DayView() {
-  return <div>DayView</div>;
+  const [showProfile, setShowProfile] = useState(true);
+  const [showTextSection, setShowTextSection] = useState(false);
+
+  const handleAddTextClick = () => {
+    setShowTextSection(true);
+  };
+
+  const handleCloseTextClick = () => {
+    setShowTextSection(false);
+  };
+
+  return (
+    <div>
+      {!showTextSection && (
+        <button onClick={handleAddTextClick}>
+          <p>Add a new Text Entry for today</p>
+        </button>
+      )}
+      {showTextSection && (
+        <div>
+          <button onClick={handleCloseTextClick}>
+            <p>Close</p>
+          </button>
+          <NewText />
+        </div>
+      )}
+      DayView
+      <div></div>
+    </div>
+  );
 }
 
 export default DayView;
