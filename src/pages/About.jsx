f0dec9b1349b5a46189c2159326932fb6f265f32
Lena Imdahl <mail@lenaimdahl.de>
2023-07-22 20:39:20 +0200

    changed about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,52 @@
-import Preview from "../component/Preview";
-
 function About() {
-
   return (
     <div className="about-page">
-        <Preview />
+      <div className="preview-container">
+        <div className="preview-features-container">
+          <span>
+            <p id="preview-emoji">🙍‍♀️🙍‍♂️</p>
+            <h4>Add a friend!</h4>
+            <p>
+              You can add a friend to your friendlist in the friend zone. So you
+              can add the friend when creating a event.
+            </p>
+          </span>
+          <span>
+            <p id="preview-emoji">🍸</p>
+            <h4>Create an event!</h4>
+            <p>
+              Create a event. You can oick a date, add a friend and choose
+              points for doing the event.
+            </p>
+          </span>
+          <span>
+            <p id="preview-emoji">💯</p>
+            <h4>count points!</h4>
+            <p>
+              The points of your weekly events are being counted, so you can
+              track, if you got you own created goal for the week.
+            </p>
+          </span>
+          <span>
+            <p id="preview-emoji">🙂</p>
+            <h4>select your mood!</h4>
+            <p>
+              Select you mood dayle and you can see how you mood is developing
+              the last two weeks. You can actually compare two weeks to get a
+              better idea.
+            </p>
+          </span>
+          <span>
+            <p id="preview-emoji">📝</p>
+            <h4>take notes!</h4>
+            <p>
+              You can take notes. Maybe something special will be happening, or
+              alrady did. At the Day vie you can see all notes and meeting for a
+              date you picked.
+            </p>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
