d8f2417771e9418084a9229ebfc0c02ba1cb7ba2
olgam <olgamartyniuk88@gmail.com>
2023-07-26 17:52:45 +0200

    mobile navbar update

--- src/component/NewNav.jsx
+++ src/component/NewNav.jsx
@@ -25,19 +25,27 @@ function NewNav() {
         {isLoggedIn ? (
           <ul>
             <li>
-              <Link to={"/about"} onClick={showNavbar}>About</Link>
+              <Link to={"/about"} onClick={showNavbar}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to={"/profile"} onClick={showNavbar}>Calendar</Link>
+              <Link to={"/profile"} onClick={showNavbar}>
+                Calendar
+              </Link>
             </li>
             <li>
-              <Link to={"/day-view"} onClick={showNavbar}>Day View</Link>
+              <Link to={"/day-view"} onClick={showNavbar}>
+                Day View
+              </Link>
             </li>
             <li>
-              <Link to={"/weekly-mood"} onClick={showNavbar}>Weekly mood</Link>
+              <Link to={"/weekly-mood"} onClick={showNavbar}>
+                Weekly mood
+              </Link>
             </li>
             <li>
-              <Link to="#" onClick={handleLogout} >
+              <Link to="#" onClick={handleLogout}>
                 Log out
               </Link>
             </li>
@@ -45,16 +53,24 @@ function NewNav() {
         ) : (
           <ul>
             <li>
-              <Link to={"/"} onClick={showNavbar}>Home</Link>
+              <Link to={"/"} onClick={showNavbar}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to={"/about"} onClick={showNavbar}>About</Link>
+              <Link to={"/about"} onClick={showNavbar}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to={"/login"} onClick={showNavbar}>Login</Link>
+              <Link to={"/login"} onClick={showNavbar}>
+                Login
+              </Link>
             </li>
             <li>
-              <Link to={"/signup"} onClick={showNavbar}>Signup</Link>
+              <Link to={"/signup"} onClick={showNavbar}>
+                Signup
+              </Link>
             </li>
           </ul>
         )}
@@ -62,9 +78,13 @@ function NewNav() {
           <FaTimes />
         </button>
       </nav>
-      <button onClick={showNavbar} className="nav-btn nav-btn-bars">
-        <FaBars className="btn-bars-icon"/>
-      </button>
+      <div className="hamburger-menu">
+        <h3>FunFair 🥳</h3>
+        <button onClick={showNavbar} className="nav-btn nav-btn-bars">
+          {/* <p>FunFair</p> */}
+          <FaBars className="btn-bars-icon" />
+        </button>
+      </div>
     </header>
   );
 }
